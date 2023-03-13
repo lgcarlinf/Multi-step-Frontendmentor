1266995ebaea4689a6651b7999e461f08c2a3a47
luiggy <luiggylml@gmail.com>
2023-03-12 20:28:22 -0500

    refactor

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const Checkbox = ({ isCheck, setIsCheck }) => {
   return (
-    <label className="custom-checkbox cursor-pointer">
-      <input
-        type="checkbox"
-        checked={isCheck}
-        value={isCheck}
-        onChange={setIsCheck}
-      />
-      <span className="checkmark"></span>
-    </label>
+    <>
+      <label className="custom-checkbox cursor-pointer">
+        <input
+          type="checkbox"
+          checked={isCheck}
+          value={isCheck}
+          onChange={setIsCheck}
+        />
+        <span className="checkmark"></span>
+      </label>
+    </>
   );
 };
 
